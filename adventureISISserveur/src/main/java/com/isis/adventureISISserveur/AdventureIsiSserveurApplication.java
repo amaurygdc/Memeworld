66eaa96112e8312d4733a9edd950e52ffd3b3947
amaurygdc <amaury.goncalves_de_carvalho@etud.univ-jfc.fr>
2020-03-03 11:49:54 +0100

    first commit

---
+++ adventureISISserveur/src/main/java/com/isis/adventureISISserveur/AdventureIsiSserveurApplication.java
@@ -0,0 +1,13 @@
+package com.isis.adventureISISserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureIsiSserveurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureIsiSserveurApplication.class, args);
+	}
+
+}
---
+++ adventureISISserveur/src/test/java/com/isis/adventureISISserveur/AdventureIsiSserveurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isis.adventureISISserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureIsiSserveurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
