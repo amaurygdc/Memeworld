9e667f9fe68174fd02fd247fd4c1eb1a5b976d1a
cesarperrincocon <cesar.perrincocon@etud.univ-jfc.fr>
2020-03-19 12:09:41 +0100

    Modifs webservices et service

--- adventureISISserveur/src/main/java/com/isis/adventureISISserveur/Webservices.java
+++ adventureISISserveur/src/main/java/com/isis/adventureISISserveur/Webservices.java
@@ -5,17 +5,26 @@
  */
 package com.isis.adventureISISserveur;
 
+import com.google.gson.Gson;
 import generated.PallierType;
 import generated.ProductType;
+import generated.World;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 /**
  *
@@ -34,22 +43,60 @@ public class Webservices {
     @GET
     @Path("world")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getXml(@Context HttpServletRequest request) {
+    public Response getXml(@Context HttpServletRequest request) throws JAXBException, FileNotFoundException {
         String username = request.getHeader("X-user");
         return Response.ok(services.getWorld(username)).build();
     }
 
     @PUT
-    @Path("poduct")
+    @Path("product")
     @Consumes({MediaType.APPLICATION_JSON})
-    public Response editProduct (String data, ProductType product) {
-        return Response.ok(services.updateProduct(data, product)).build();
+    public void editProduct(String data, String username) throws FileNotFoundException, JAXBException {
+        ProductType product = new Gson().fromJson(data, ProductType.class);
+        services.updateProduct(username, product);
     }
 
     @PUT
-    @Path("poduct")
+    @Path("manager")
     @Consumes({MediaType.APPLICATION_JSON})
-    public Response editManager (String data, PallierType manager ) {
-        return Response.ok(services.updateManager(data, manager)).build();
+    public void editManager(String data, String username) throws FileNotFoundException, JAXBException {
+        PallierType manager = new Gson().fromJson(data, PallierType.class);
+        services.updateManager(username, manager);
+
+    }
+    
+    @PUT
+    @Path("upgrade")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void editUpgrade(String data, String username) throws JAXBException, FileNotFoundException{
+        PallierType upgrade = new Gson().fromJson(data, PallierType.class);
+        services.updateUpgrade(username, upgrade);
+    }
+    @PUT
+    @Path("angelupgrade")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void editAngelUpgrade(String data, String username) throws JAXBException, FileNotFoundException{
+        PallierType upgrade = new Gson().fromJson(data, PallierType.class);
+        services.updateUpgrade(username, upgrade);
+    }
+    
+    @DELETE
+    @Path("world")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void resetWorld(String username) throws FileNotFoundException, JAXBException{
+        World w = services.readWorldFromXml(username);
+        double scoreToKeep = w.getScore();
+        double totalangels = w.getTotalangels();
+        double activeangels = 150 * Math.sqrt(w.getScore()/Math.pow(10, 15))-totalangels;   
+        JAXBContext jaxbContext;
+
+        InputStream input = getClass().getClassLoader().getResourceAsStream("world.xml");
+        jaxbContext = JAXBContext.newInstance(World.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        World newWorld = (World) jaxbUnmarshaller.unmarshal(input);
+        newWorld.setScore(scoreToKeep);
+        newWorld.setTotalangels(totalangels + activeangels);
+        newWorld.setActiveangels(activeangels);
+        services.saveWorldToXml(newWorld, username);
     }
 }
